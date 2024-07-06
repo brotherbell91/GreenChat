ce637dfcdd777c2c2dfe54fecbb794e1fcf06421
Bell <lhj0412@ucware.net>
2024-07-06 23:44:12 +0900

    MessageCompose.kt 이미지, 메시지 제목 정렬

--- app/src/main/java/com/greenchat/compose/MessageCompose.kt
+++ app/src/main/java/com/greenchat/compose/MessageCompose.kt
@@ -1,6 +1,7 @@
 package com.greenchat.compose
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -23,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -72,14 +75,16 @@ fun MessageScreen(messageData: MessageData) {
                                         ) {
                                             Row(
                                                 horizontalArrangement = Arrangement.Start,
+                                                verticalAlignment = Alignment.CenterVertically
                                             ) {
                                                 Image(
                                                     painter = painterResource(id = messageData.imageRes),
                                                     contentDescription = null,
-                                                    contentScale = ContentScale.Crop,
                                                     modifier = Modifier
                                                         .size(70.dp)
-                                                        .padding(8.dp)
+                                                        .clip(CircleShape)
+                                                        .background(Color.Transparent),
+                                                    contentScale = ContentScale.Crop,
                                                 )
                                                 Column(
                                                     Modifier.fillMaxWidth(),
@@ -142,7 +147,8 @@ fun MessageScreen(messageData: MessageData) {
                                             }
                                             Spacer(modifier = Modifier.height(4.dp))
                                             Row(
-                                                verticalAlignment = Alignment.CenterVertically,
+                                                horizontalArrangement = Arrangement.Start,
+                                                verticalAlignment = Alignment.CenterVertically
                                             ) {
                                                 Text(
                                                     text = "Subject: ",
