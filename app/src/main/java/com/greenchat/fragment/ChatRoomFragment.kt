e0408db9fd3d85f81d79f730dbcf998437c6ac18
Bell <lhj0412@ucware.net>
2024-07-07 00:22:07 +0900

    ChatRoomFragment.kt 초기 셋팅 추가

---
+++ app/src/main/java/com/greenchat/fragment/ChatRoomFragment.kt
@@ -0,0 +1,31 @@
+package com.greenchat.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+
+class ChatRoomFragment : Fragment() {
+
+    companion object {
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+
+
+
+
+            }
+        }
+    }
+}
