88e8b4333cf3f3e44a91f1e98d3321c136a6d919
Bell <lhj0412@ucware.net>
2024-07-07 00:18:34 +0900

    ChatData.kt 추가

---
+++ app/src/main/java/com/greenchat/data/ChatData.kt
@@ -0,0 +1,16 @@
+package com.greenchat.data
+
+import java.time.LocalDateTime
+
+data class ChatData(
+    var id: Int = -1,
+    var imageRes: Int = -1,
+    var name: String = "",
+    var unreadCount: Int = -1,
+    var time: LocalDateTime = LocalDateTime.now(),
+    var content: String = "",
+) {
+    companion object{
+
+    }
+}
