e7a1b5e6390c556fc1a258fef26dc7251eb74d30
Bell <lhj0412@ucware.net>
2024-07-08 23:12:52 +0900

    ChatRoomListData 임시 데이터 일부 제거

--- app/src/main/java/com/greenchat/data/ChatRoomListData.kt
+++ app/src/main/java/com/greenchat/data/ChatRoomListData.kt
@@ -25,16 +25,6 @@ data class ChatRoomListData(
             ChatRoomListData(6, R.drawable.profile, "Chat Room 6", 8, LocalDateTime.of(2022, 6,7, 8, 16,32 ), "Last message 3", 0),
             ChatRoomListData(7, R.drawable.profile, "Chat Room 7", 10, LocalDateTime.now(), "Last message 4", 0),
             ChatRoomListData(8, R.drawable.profile, "Chat Room 8", 5, LocalDateTime.now(), "Last message 5", 0),
-            ChatRoomListData(9, R.drawable.profile, "Chat Room 9", 8, LocalDateTime.now(), "Last message 6", 2),
-            ChatRoomListData(10, R.drawable.profile, "Chat Room 10", 10, LocalDateTime.now(), "Last message 7", 1),
-            ChatRoomListData(11, R.drawable.profile, "Chat Room 11", 5, LocalDateTime.now(), "Last message 8", 0),
-            ChatRoomListData(12, R.drawable.profile, "Chat Room 12", 8, LocalDateTime.now(), "Last message 9", 3),
-            ChatRoomListData(13, R.drawable.profile, "Chat Room 13", 10, LocalDateTime.now(), "Last message 10", 0),
-            ChatRoomListData(14, R.drawable.profile, "Chat Room 14", 5, LocalDateTime.now(), "Last message 11", 0),
-            ChatRoomListData(15, R.drawable.profile, "Chat Room 15", 8, LocalDateTime.now(), "Last message 12", 0),
-            ChatRoomListData(16, R.drawable.profile, "Chat Room 16", 10, LocalDateTime.now(), "Last message 13", 0),
-            ChatRoomListData(17, R.drawable.profile, "Chat Room 17", 5, LocalDateTime.now(), "Last message 14", 5),
-            ChatRoomListData(18, R.drawable.profile, "Chat Room 18", 8, LocalDateTime.now(), "Last message 15", 0),
         )
 
         val openChatRooms = listOf(
