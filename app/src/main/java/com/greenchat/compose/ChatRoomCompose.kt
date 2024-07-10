ea4575e7f858d001e8bb8babff3cc6ae0b1dcea2
Bell <lhj0412@ucware.net>
2024-07-11 00:35:21 +0900

    ChatRoomCompose.kt 채팅 입력 칸 추가 - 채팅 입력 시 색상 초록색으로 변경 적용 - 채팅 보내기 아이콘 추가 - 채팅에 바텀시트 버튼 추가

--- app/src/main/java/com/greenchat/compose/ChatRoomCompose.kt
+++ app/src/main/java/com/greenchat/compose/ChatRoomCompose.kt
@@ -1,7 +1,9 @@
 package com.greenchat.compose
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,30 +16,48 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.greenchat.R
 import com.greenchat.data.ChatData
 import com.greenchat.data.ChatRoomData
 import com.greenchat.ui.colorPrimary
 import com.greenchat.ui.ghost_white
+import com.greenchat.ui.image_gray
+import com.greenchat.ui.text_hint_color
 import com.greenchat.util.Constants
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -69,6 +89,26 @@ fun ChatRoomScreen(chatRoomData : ChatRoomData) {
                                 ChatMessageBubble(chatData = chatData)
                             }
                         }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.Bottom,
+                    ) {
+                        ChatRoomCustomStyleTextField(
+                            placeHolder = "Type a message...",
+                            leadingIconId = R.drawable.plus,
+                            sendIconId = R.drawable.chat_send,
+                            keyboardType = KeyboardType.Text,
+                            visualTransformation = VisualTransformation.None,
+                            onIconClick = {
+                                // 추가 아이콘 클릭 시 동작 추가
+                            },
+                            onSendMessage = { message ->
+                                // 메시지를 보낼 때 동작 추가
+                            }
+                        )
                     }
                 }
             },
@@ -174,6 +214,92 @@ fun ChatMessageBubble(chatData: ChatData) {
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatRoomCustomStyleTextField(
+    placeHolder: String,
+    leadingIconId: Int,
+    sendIconId: Int,
+    keyboardType: KeyboardType,
+    visualTransformation: VisualTransformation,
+    onIconClick: () -> Unit,
+    onSendMessage: (String) -> Unit
+) {
+    val textState = remember { mutableStateOf(TextFieldValue()) }
+
+    Card(
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            value = textState.value,
+            onValueChange = { valueChanged ->
+                textState.value = valueChanged
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+            placeholder = { Text(text = placeHolder, color = text_hint_color) },
+            leadingIcon = {
+                Row(
+                    modifier = Modifier.wrapContentWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = {
+                        Image(
+                            modifier = Modifier
+                                .padding(end = 8.dp)
+                                .size(23.dp)
+                                .clickable(onClick = onIconClick),
+                            bitmap = ImageBitmap.imageResource(id = leadingIconId),
+                            colorFilter = ColorFilter.tint(colorPrimary),
+                            contentDescription = "custom_text_field"
+                        )
+                        Canvas(
+                            modifier = Modifier.height(24.dp)
+                        ) {
+                            drawLine(
+                                color = Color.LightGray,
+                                start = Offset(0f, 0f),
+                                end = Offset(0f, size.height),
+                                strokeWidth = 2.0F
+                            )
+                        }
+                    }
+                )
+            },
+            trailingIcon = {
+                Image(
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .size(23.dp)
+                        .clickable {
+                            onSendMessage(textState.value.text)
+                            textState.value = TextFieldValue() //채팅 메시지 초기화
+                        },
+                    bitmap = ImageBitmap.imageResource(id = sendIconId),
+                    colorFilter = ColorFilter.tint(
+                        if(textState.value.text.isNotEmpty()) colorPrimary else image_gray
+                    ),
+                    contentDescription = "send_message_icon"
+                )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = colorPrimary,
+                unfocusedBorderColor = Color.Transparent,
+                focusedLabelColor = Color.White,
+                focusedTrailingIconColor = Color.White,
+            ),
+            shape = RoundedCornerShape(20.dp),
+            textStyle = TextStyle(color = Color.Black, fontSize = 16.sp),
+            visualTransformation = visualTransformation
+        )
     }
 }
 
