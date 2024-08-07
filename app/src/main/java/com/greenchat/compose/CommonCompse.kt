cf33817a90ff4788b8b2a5c2d4a66feed31da6cc
Bell <lhj0412@ucware.net>
2024-08-08 00:00:55 +0900

    공통 플로팅 버튼 메서드 추가

--- app/src/main/java/com/greenchat/compose/CommonCompse.kt
+++ app/src/main/java/com/greenchat/compose/CommonCompse.kt
@@ -2,9 +2,16 @@ package com.greenchat.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -105,4 +113,28 @@ fun todayCheck(time: LocalDateTime) : String{
         isThisYear -> time.format(monthDayFormatter)
         else -> time.format(dateFormatter)
     }
+}
+
+@Composable
+fun FloatingButton(onClick: () -> Unit, image : ImageVector){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        FloatingActionButton(
+            onClick = { onClick() },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(32.dp),
+            containerColor = colorPrimary,
+            contentColor = Color.White,
+        ) {
+            Icon(image, "Icon.")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFloatingButton(){
+    FloatingButton(onClick = {}, Icons.Default.Edit )
 }
