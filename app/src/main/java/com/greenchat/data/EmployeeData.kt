abf120dabbf035b31ab50f4c50f0dd870e5f033d
Bell <lhj0412@ucware.net>
2024-08-10 18:54:56 +0900

    EmployeeData.kt에 나의정보, 빈 데이터값, 예시데이터값 추가

--- app/src/main/java/com/greenchat/data/EmployeeData.kt
+++ app/src/main/java/com/greenchat/data/EmployeeData.kt
@@ -1,6 +1,7 @@
 package com.greenchat.data
 
 import android.os.Parcelable
+import com.greenchat.R
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -11,4 +12,10 @@ data class EmployeeData(
     var department: String = "",
     var position: String = "",
     var imageRes: Int = -1,
-) : Parcelable
+) : Parcelable {
+    companion object {
+        val myData = EmployeeData(id = "lhj", name = "이형종", phone = "010-0000-0000", department = "Develop1", position = "사원", imageRes = R.drawable.profile)
+        val emptyData = EmployeeData(id = "", name = "", phone = "", department = "", position = "", imageRes = 1)
+        val employeeData = EmployeeData(id = "hgd", name = "홍길동", phone = "010-0000-0000", department = "Develop2", position = "사원", imageRes = R.drawable.profile)
+    }
+}
