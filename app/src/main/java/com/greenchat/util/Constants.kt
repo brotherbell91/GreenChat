0d9c5b9ddfebc6b9a7a752ab99914f1a1482cb7d
Bell <lhj0412@ucware.net>
2024-08-20 21:20:40 +0900

    커스텀 탑바 타이틀 상수값 지정

--- app/src/main/java/com/greenchat/util/Constants.kt
+++ app/src/main/java/com/greenchat/util/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val MESSAGE_FRAGMENT_TAG = "MessageFragment"
     const val PROFILE_FRAGMENT_TAG = "ProfileFragment"
     const val MESSAGE_EDIT_FRAGMENT_TAG = "MessageEditFragment"
+    const val SELECT_BUDDY_FRAGMENT_TAG = "SelectBuddyFragment"
+
+    const val SEND_MESSAGE_TITLE = "Send Message"
+    const val SELECT_BUDDY_TITLE = "Select Buddy"
 }
