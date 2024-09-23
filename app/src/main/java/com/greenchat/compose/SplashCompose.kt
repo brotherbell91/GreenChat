33d6dfed8a2ea27ab4bd7b9adcc5d63a8266964d
Bell <lhj0412@ucware.net>
2024-09-23 11:58:19 +0900

    SplashScreen 데이터 수신을 위한 비동기 선행 작업

--- app/src/main/java/com/greenchat/compose/SplashCompose.kt
+++ app/src/main/java/com/greenchat/compose/SplashCompose.kt
@@ -1,32 +1,52 @@
 package com.greenchat.compose
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.greenchat.R
 import com.greenchat.navigation.Screen
-import kotlinx.coroutines.CoroutineScope
+import com.greenchat.util.Constants
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    val coroutineScope = rememberCoroutineScope()
+    var isVisible by remember { mutableStateOf(false) }
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Splash Screen")
-        splashCoroutine(navController, coroutineScope)
+    LaunchedEffect(Unit) {
+        isVisible = true
+        delay(1000)
+        navController.navigate(Screen.Home.route) {
+            popUpTo(Screen.Home.route) { inclusive = true }
+        }
     }
+    Splash(isVisible = isVisible)
 }
 
-fun splashCoroutine(navController: NavController, coroutineScope: CoroutineScope){
-    coroutineScope.launch {
-        delay(1000)
-        navController.navigate(Screen.Home.route)
+@Composable
+fun Splash(isVisible: Boolean) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = fadeIn(animationSpec = tween(Constants.SPLASH_DELAY_TIME))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.greenchat_logo_green),
+                contentDescription = "Logo",
+                modifier = Modifier.size(200.dp)
+            )
+        }
     }
-
 }
--- app/src/main/java/com/greenchat/util/Constants.kt
+++ app/src/main/java/com/greenchat/util/Constants.kt
@@ -3,4 +3,6 @@ package com.greenchat.util
 object Constants {
     const val SEND_MESSAGE_TITLE = "Send Message"
     const val SELECT_BUDDY_TITLE = "Select Buddy"
+
+    const val SPLASH_DELAY_TIME = 1000
 }
