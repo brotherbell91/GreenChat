a011d0217f5a19cbcc93701fa17c4b8bc587c6c8
Bell <lhj0412@ucware.net>
2024-09-25 00:59:59 +0900

    ChatData 키값 제거

--- app/src/main/java/com/greenchat/data/ChatData.kt
+++ app/src/main/java/com/greenchat/data/ChatData.kt
@@ -1,13 +1,9 @@
 package com.greenchat.data
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
-@Entity(tableName = "chat")
 data class ChatData(
-    @PrimaryKey(autoGenerate = true) @JvmField val id: Int = 0,
-//    var id: Int = -1,
+    var id: Int = -1,
     var imageRes: Int = -1,
     var name: String = "",
     var unreadCount: Int = -1,
