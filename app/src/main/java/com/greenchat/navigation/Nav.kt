d469ce61616f275864021e2b27766b0d1e5910df
Bell <hjgudwhd123@gmail.com>
2024-10-07 20:12:28 +0900

    Home 라운드 적용

--- app/src/main/java/com/greenchat/navigation/Nav.kt
+++ app/src/main/java/com/greenchat/navigation/Nav.kt
@@ -1,14 +1,20 @@
 package com.greenchat.navigation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -26,6 +32,7 @@ import com.greenchat.compose.SplashScreen
 import com.greenchat.data.ChatRoomData
 import com.greenchat.data.EmployeeData
 import com.greenchat.data.MessageData
+import com.greenchat.ui.colorPrimary
 import com.greenchat.ui.ghost_white
 import com.greenchat.viewmodel.MyViewModel
 
@@ -67,20 +74,32 @@ fun NavigationHome(
     Scaffold(
         topBar = { CustomTopAppBar(false, onClose = {}) },
         bottomBar = { CustomBottomBar(navController) }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = Screen.Organization.route,
+    ) { paddingValues ->
+        Surface(
             modifier = Modifier
-                .padding(innerPadding)
-                .background(ghost_white)
+                .fillMaxSize()
+                .padding(paddingValues),
+            color = colorPrimary
         ) {
-            composable(Screen.Organization.route) { OrganizationScreen(onEmployeeSelected, viewModel) }
-            composable(Screen.ChatList.route) { ChatRoomListScreen(onChatRoomSelected, viewModel)
-                FloatingButton(openDashboard = {onFloatingChatRoomSelected()}, image = Icons.Default.Add)}
-            composable(Screen.MessageList.route) { MessageListScreen(onMessageSelected, viewModel)
-                FloatingButton(openDashboard = {onFloatingMessageSelected()}, image = Icons.Default.Edit)}
-            composable(Screen.More.route) { MoreScreen(viewModel) }
+            Card(
+                colors = CardDefaults.cardColors(containerColor = ghost_white),
+                shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp),
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.Organization.route,
+                    modifier = Modifier
+                        .background(ghost_white)
+                ) {
+                    composable(Screen.Organization.route) { OrganizationScreen(onEmployeeSelected, viewModel) }
+                    composable(Screen.ChatList.route) { ChatRoomListScreen(onChatRoomSelected, viewModel)
+                        FloatingButton(openDashboard = {onFloatingChatRoomSelected()}, image = Icons.Default.Add)}
+                    composable(Screen.MessageList.route) { MessageListScreen(onMessageSelected, viewModel)
+                        FloatingButton(openDashboard = {onFloatingMessageSelected()}, image = Icons.Default.Edit)}
+                    composable(Screen.More.route) { MoreScreen(viewModel) }
+                }
+            }
         }
     }
 }
